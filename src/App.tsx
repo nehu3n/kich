8402a51268ebd68412d9df2e5dfc2a138e44d539
nehu3n <rashid.developing@gmail.com>
2024-04-27 21:04:52 -0300

    feat: Sidebar design.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import SideBarComponent from "./components/Navbar";
 import "./styles.css";
 
 function App() {
   return (
-    <div>
+    <div className="flex h-screen bg-gray-50">
+      <div className="bg-white shadow-md">
+        <SideBarComponent />
+      </div>
+      <div className="flex-1 p-4">
+        <h1 className="text-3xl font-extrabold text-gray-800 mt-5 ml-3">Accounts</h1>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
