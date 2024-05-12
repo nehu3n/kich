c6aa2e128d5fdbfa1d8dd3cff4ecf5d870200bf8
nehu3n <rashid.developing@gmail.com>
2024-05-12 01:05:49 -0300

    feat: Encryption functions.

---
+++ src-tauri/src/internal/crypto.rs
@@ -0,0 +1,44 @@
+use aes_gcm::{
+    aead::{Aead, AeadCore, KeyInit, OsRng},
+    Aes256Gcm, Key, Nonce,
+};
+use serde::{Deserialize, Serialize};
+use serde_json;
+
+pub fn encrypt<'a, T: Serialize>(key_str: &'a str, plaintext: &'a T) -> Result<String, String> {
+    let key = Key::<Aes256Gcm>::from_slice(key_str.as_bytes());
+    let nonce = Aes256Gcm::generate_nonce(&mut OsRng);
+
+    let cipher = Aes256Gcm::new(key);
+
+    let plaintext_json = serde_json::to_string(plaintext).map_err(|e| e.to_string())?;
+    let ciphered_data = cipher
+        .encrypt(&nonce, plaintext_json.as_bytes())
+        .map_err(|e| e.to_string())?;
+
+    let mut encrypted_data: Vec<u8> = nonce.to_vec();
+    encrypted_data.extend_from_slice(&ciphered_data);
+
+    Ok(hex::encode(encrypted_data))
+}
+
+pub fn decrypt<T: for<'a> Deserialize<'a>>(
+    key_str: &str,
+    encrypted_data: &str,
+) -> Result<T, String> {
+    let encrypted_data = hex::decode(encrypted_data).map_err(|e| e.to_string())?;
+
+    let key = Key::<Aes256Gcm>::from_slice(key_str.as_bytes());
+
+    let (nonce_arr, ciphered_data) = encrypted_data.split_at(12);
+    let nonce = Nonce::from_slice(nonce_arr);
+
+    let cipher = Aes256Gcm::new(key);
+
+    let plaintext = cipher
+        .decrypt(nonce, ciphered_data)
+        .map_err(|e| e.to_string())?;
+    let plaintext_str = String::from_utf8(plaintext).map_err(|e| e.to_string())?;
+
+    serde_json::from_str(&plaintext_str).map_err(|e| e.to_string())
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,14 +1,38 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+mod internal {
+    pub mod crypto;
 }
+
+// use internal::crypto::{decrypt, encrypt};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Account {
+    username: String,
+    email: String,
+    password: String,
+    phone: String,
+    notes: Vec<String>,
+    tags: Vec<String>,
+}
+
+/*
+#[tauri::command]
+fn encrypt_<T: Serialize>(key_str: &str, plaintext: T) -> Result<String, String> {
+    encrypt(key_str, &plaintext)
+}
+
+#[tauri::command]
+fn decrypt_(key_str: &str, encrypted_data: &str) -> Result<Account, String> {
+    decrypt(key_str, encrypted_data)
+}
+*/
 
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
